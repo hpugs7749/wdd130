2ffc0c00920e8fc95d3f577ba525a65c2ab3b63c
hpugs7749 <hpugsley@byupathway.edu>
2025-01-18 22:18:37 -0700

    Testing :)

---
+++ Week 2 Notes/week2_notes.py
@@ -0,0 +1,12 @@
+'''
+SEMANTIC VS NON-SEMANTIC
+This is my notes for my second week of html learning. The first interesting fact that I have learned
+is the difference between semantic & non-semantic elements. Semantic elements to html si what provides
+the specific meaning and structure to html coding. Non-semantic elemnts are more involved with classing
+and iding attributes within the code.
+
+
+
+
+
+'''
